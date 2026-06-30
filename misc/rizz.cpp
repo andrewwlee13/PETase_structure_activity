41e7a1d05df8dee3d206d6ae8cbbdbb794638572
andrewwlee13 <andreww.lee13@berkeley.edu>
2026-06-29 18:02:17 -0700

    stuff

---
+++ misc/rizz.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+        string firstName;
+        cout << "what is Angel Melchor's first name? ";
+        cin >> firstName;
+
+
+        string lastName;
+        cout << "what is Angel Melchor's last name? ";
+        cin >> lastName;
+
+        cout << "Angel Melchor's first and last names are " << firstName << " and " << lastName << ", respectively.";
+
+        return 0;
+}
+
+
+
+
